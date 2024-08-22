c15628568765dff147b3744043332382fb857192
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-08-23 01:26:28 +1200

    Comments, and Once

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ export namespace IPC {
     args: any[]
   }
 
+  /** Sends an `invoke` message through IPC, and expects a result asynchronously. */
   export function invoke(channel: string, ...args: any[]): Promise<any> {
     const data: InvokeEventData = {
       channel: channel,
@@ -34,6 +35,7 @@ export namespace IPC {
     })
   }
 
+  /** Adds a handler for an `invoke` IPC. This handler will be called whenever `invoke(channel, ...args)` is called */
   export function handle(channel: string, listener: (...args: any[]) => any) {
     server.system.afterEvents.scriptEventReceive.subscribe(event => {
       if (event.id === 'ipc:invoke') {
@@ -50,6 +52,7 @@ export namespace IPC {
     })
   }
 
+  /** Sends a message with `args` to `channel` */
   export function send(channel: string, ...args: any[]): void {
     const data: SendEventData = {
       channel: channel,
@@ -58,12 +61,26 @@ export namespace IPC {
     server.world.getDimension('overworld').runCommand(`/scriptevent ipc:send ${JSON.stringify(data)}`)
   }
 
+  /** Listens to `channel`. When a new message arrives, `listener` will be called with `listener(args)`. */
   export function on(channel: string, listener: (...args: any[]) => void) {
     server.system.afterEvents.scriptEventReceive.subscribe(event => {
       if (event.id === 'ipc:send') {
         const send_data = JSON.parse(event.message) as SendEventData
         if (send_data.channel === channel) {
           listener(send_data.args)
+        }
+      }
+    })
+  }
+
+  /** Listens to `channel` once. When a new message arrives, `listener` will be called with `listener(args)`, and then removed. */
+  export function once(channel: string, listener: (...args: any[]) => void) {
+    const event_listener = server.system.afterEvents.scriptEventReceive.subscribe(event => {
+      if (event.id === 'ipc:send') {
+        const send_data = JSON.parse(event.message) as SendEventData
+        if (send_data.channel === channel) {
+          listener(send_data.args)
+          server.system.afterEvents.scriptEventReceive.unsubscribe(event_listener)
         }
       }
     })
