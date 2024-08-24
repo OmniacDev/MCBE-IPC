d51a48de4b0c38d67771e4128b7edb6aeab1ee69
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-08-25 02:48:49 +1200

    Serialize & Deserialize

--- src/serialization.ts
+++ src/serialization.ts
@@ -3,12 +3,25 @@ let ID = 0
 
 type ChunkData = [number, number, string]|[number, number, string, boolean]
 
-export function serialize(data: any) {
-  return JSON.stringify(data);
+export function serialize(data: any): string[] {
+  return chunk(JSON.stringify(data)).map((data_chunk) => {
+    return JSON.stringify(data_chunk)
+  })
 }
 
-export function deserialize(data: string) {
-  return JSON.parse(data);
+export function deserialize(chunks: string[]) {
+  const data = new Map<number, string[]>()
+  chunks.map((str_chunk) => {
+    const chunk_data = JSON.parse(str_chunk) as ChunkData
+    const map_data = data.get(chunk_data[0])
+    if (map_data !== undefined) {
+      map_data[chunk_data[1]] = chunk_data[2]
+    }
+  })
+
+  return Array.from(data.values()).map((data_arr) => {
+    data_arr.join('')
+  })
 }
 
 export function chunk(data: string): ChunkData[] {
