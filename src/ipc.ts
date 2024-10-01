f6d7e22f9c936529c0fe18512a1f848c682a6563
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-10-01 20:19:51 +1300

    optimised auto-sizing

--- src/ipc.ts
+++ src/ipc.ts
@@ -288,81 +288,50 @@ namespace IPC {
   }
 
   function emit(event_id: string, channel: string, args: any[]) {
-    function CMD(payload: Payload) {
-      return `scriptevent ipc:${event_id} ${encodeURI(Payload.toString(payload))}`
+    const CMD = (payload: Payload) => `scriptevent ipc:${event_id} ${encodeURI(Payload.toString(payload))}`
+    const cmd = CMD({ channel: channel, id: ID, data: '' })
+    const args_str = JSON.stringify(args)
+    const chars = Array.from(args_str)
+    const enc_chars = (function (chars: string[]) {
+      const r: string[] = []
+      let acc: string = ''
+      chars.forEach(c => {
+        if (c === '\\') {
+          acc += c
+          if (acc.length === 2) {
+            r.push(encodeURI(acc))
+            acc = ''
+          }
+        } else if (acc.length === 1) {
+          r.push(encodeURI(acc + c))
+          acc = ''
+        } else {
+          r.push(encodeURI(c))
+        }
+      })
+      return r
+    })(Array.from(JSON.stringify(args_str)))
+
+    const commands: string[] = []
+    let str = ''
+    let enc_str_len = 0
+    for (let i = 0; i < chars.length; i++) {
+      const enc_char = enc_chars[i + 1]
+      const cmd_len = enc_str_len + enc_char.length + cmd.length + `,${commands.length},1`.length
+      if (cmd_len < CONFIG.FRAGMENTATION.MAX_CMD_LENGTH) {
+        str += chars[i]
+        enc_str_len += enc_char.length
+      } else {
+        commands.push(CMD({ channel: channel, id: ID, data: str, index: commands.length }))
+        str = chars[i]
+        enc_str_len = enc_char.length
+      }
     }
 
-    const args_str = JSON.stringify(args)
-    const commands: string[] = []
-
-    let idx = 0
-    while (idx != -1) {
-      let sub_str = args_str.substring(idx)
-
-      // create one cmd, then add extra length for mid & final
-      const cmd = CMD({ channel: channel, id: ID, data: sub_str })
-
-      // first command, so try to create a single
-      if (commands.length === 0) {
-        if (cmd.length < CONFIG.FRAGMENTATION.MAX_CMD_LENGTH) {
-          commands.push(cmd)
-          idx = -1
-          break
-        }
-      }
-      // not first, so try to create final
-      else {
-        const final_length = cmd.length + `,${commands.length},1`.length
-        if (final_length < CONFIG.FRAGMENTATION.MAX_CMD_LENGTH) {
-          commands.push(CMD({ channel: channel, id: ID, data: sub_str, index: commands.length, final: true }))
-          idx = -1
-          break
-        }
-      }
-
-      // if neither worked, create a mid
-      const mid_length = cmd.length + `,${commands.length}`.length
-      if (mid_length > CONFIG.FRAGMENTATION.MAX_CMD_LENGTH) {
-        const chars = Array.from(sub_str)
-
-        const encoded_chars = (function (chars: string[]) {
-          const result: string[] = []
-          let accumulator: string = ''
-          chars.forEach(c => {
-            if (c === '\\') {
-              accumulator += c
-              if (accumulator.length === 2) {
-                result.push(encodeURI(accumulator))
-                accumulator = ''
-              }
-            } else if (accumulator.length === 1) {
-              result.push(encodeURI(accumulator + c))
-              accumulator = ''
-            } else {
-              result.push(encodeURI(c))
-            }
-          })
-          return result
-        })(Array.from(JSON.stringify(sub_str)))
-
-        let enc_chars_len = encoded_chars.reduce((acc, c) => acc + c.length, 0)
-        
-        const enc_target_len = enc_chars_len - (mid_length - CONFIG.FRAGMENTATION.MAX_CMD_LENGTH)
-        if (enc_target_len < 1) throw new Error('Invalid Target Length')
-
-        while (enc_chars_len > enc_target_len) {
-          chars.pop()
-          enc_chars_len -= encoded_chars[chars.length + 1]?.length
-        }
-
-        const adjusted_string = chars.join('')
-        if (adjusted_string.length < 1) throw new Error('Empty Data')
-
-        idx += adjusted_string.length
-        commands.push(CMD({ channel: channel, id: ID, data: adjusted_string, index: commands.length }))
-      } else {
-        commands.push(CMD({ channel: channel, id: ID, data: sub_str, index: commands.length }))
-      }
+    if (commands.length === 0) {
+      commands.push(CMD({ channel: channel, id: ID, data: str }))
+    } else {
+      commands.push(CMD({ channel: channel, id: ID, data: str, index: commands.length, final: true }))
     }
 
     system.runJob(
