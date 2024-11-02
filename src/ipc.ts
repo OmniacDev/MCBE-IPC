85f1d89f8eece944c65a94620752b34ea425f4ad
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-02 14:53:22 +1300

    exports

--- src/ipc.ts
+++ src/ipc.ts
@@ -326,7 +326,7 @@ namespace IPC {
 
 export default IPC
 
-export namespace SERDE {
+namespace SERDE {
   const INVALID_START_CODES = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
   const INVALID_CODES = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
@@ -387,7 +387,7 @@ export namespace SERDE {
   }
 }
 
-export namespace CRYPTO {
+namespace CRYPTO {
   export const PRIME: number = 19893121
   export const MOD: number = 341
 
