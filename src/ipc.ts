922ac17741df51ed95fdc9e3ca628b8b604f5b39
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-01-15 09:39:07 +1300

    update license & package

--- src/ipc.ts
+++ src/ipc.ts
@@ -2,7 +2,7 @@
  * @license
  * MIT License
  *
- * Copyright (c) 2024 OmniacDev
+ * Copyright (c) 2025 OmniacDev
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
